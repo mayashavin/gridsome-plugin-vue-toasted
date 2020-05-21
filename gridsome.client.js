e6ddaf7c2a4523960081071d5ac8db90eb0fb4a9
Maya Shavin <maya@cloudinary.com>
2020-05-21 12:12:58 +0300

    fix issue with require

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,6 +1,6 @@
 export default function (Vue, options) {
   if(process.isClient) {
-    const Toasted = require('vue-toasted')
+    const { default: Toasted } = require('vue-toasted')
 
     Vue.use(Toasted, options)
   }
